ef33515f4ba130246d662b1033601adf0ec15e25
Vương Hoàng Hiếu <vuonghoanghieu96@gmail.com>
2022-08-25 13:39:38 +0700

    - NLog - Timemer - Read/Write File Data - SetupProject - Configuration Manager

--- WindowsService/WinService.cs
+++ WindowsService/WinService.cs
@@ -1,25 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
+using System.Configuration;
 using System.IO;
-using System.Linq;
 using System.ServiceProcess;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace WindowsService
 {
-    public partial class WinService : ServiceBase, IDisposable
+    public partial class WinService : ServiceBase
     {
         private static readonly NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+        private readonly AppConfigs _appConfigs;
         private Timer _timer = null;
 
         public WinService()
         {
             InitializeComponent();
+            try
+            {
+                _appConfigs = ConfigurationManager.GetSection("AppConfigs") as WindowsService.AppConfigs;
+                if (_appConfigs == null)
+                {
+                    throw new Exception("App Config are not defined");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex.ToString(), ex);
+            }
         }
 
         protected override void OnStart(string[] args)
@@ -30,34 +38,82 @@ namespace WindowsService
 
         private void DoWork(object state)
         {
-            _logger.Info($"DoWork: {DateTime.Now.ToString("HH:mm:ss dd/MM/yyyy")}");
-
             try
             {
-
-                var strExeFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
-                var strWorkPath = Path.GetDirectoryName(strExeFilePath);
-                var strMaxIdFilePath = Path.Combine(strWorkPath, "StoreMaxID.txt");
-
-                _logger.Info(strMaxIdFilePath);
-
-                var maxId = int.Parse(File.ReadAllText(strMaxIdFilePath));
-
+                var maxId = ReadMaxId();
                 _logger.Info($"{maxId}");
-
                 maxId++;
-
-                File.WriteAllText(strMaxIdFilePath, $"{maxId}");
-            } catch(Exception ex)
+                WriteMaxId(maxId);
+            }
+            catch (Exception ex)
             {
                 _logger.Error(ex.ToString(), ex);
                 throw ex;
             }
         }
 
+        private int ReadMaxId()
+        {
+            var maxId = 0;
+            try
+            {
+                var dir = _appConfigs.FileConfigs.Dir;
+                var fileName = _appConfigs.FileConfigs.FileName;
+                var strMaxIdFileTempPath = Path.Combine(Path.GetTempPath(), dir, fileName);
+                var str = "";
+                if (File.Exists(strMaxIdFileTempPath))
+                {
+                    str = File.ReadAllText(strMaxIdFileTempPath);
+                }
+                else
+                {
+                    var strExeFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                    var strWorkPath = Path.GetDirectoryName(strExeFilePath);
+                    var strMaxIdFilePath = Path.Combine(strWorkPath, fileName);
+                    str = File.ReadAllText(strMaxIdFilePath);
+                }
+                try
+                {
+                    maxId = int.Parse(str);
+                }
+                catch (FormatException ex)
+                {
+                    maxId = 0;
+                    _logger.Warn(ex.ToString(), ex);
+                }
+            }
+            catch (Exception ex)
+            {
+                //_logger.Error(ex.ToString(), ex);
+                throw ex;
+            }
+            return maxId;
+        }
+
+        private void WriteMaxId(int maxId)
+        {
+            try
+            {
+                var dir = _appConfigs.FileConfigs.Dir;
+                var fileName = _appConfigs.FileConfigs.FileName;
+                var strMaxIdFileTempPath = Path.Combine(Path.GetTempPath(), dir, fileName);
+                var pathDir = Path.Combine(Path.GetTempPath(), dir);
+                if (!Directory.Exists(pathDir))
+                {
+                    Directory.CreateDirectory(pathDir);
+                }
+                File.WriteAllText(strMaxIdFileTempPath, $"{maxId}");
+            }
+            catch (Exception ex)
+            {
+                //_logger.Error(ex.ToString(), ex);
+                throw ex;
+            }
+        }
+
         protected override void OnStop()
         {
-            if(_timer != null)
+            if (_timer != null)
             {
                 _timer.Change(Timeout.Infinite, 0);
             }
